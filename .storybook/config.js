4a8cc54ada9c330205b061ff9c3c2ac32add0669
Hanz Luo <lhz0516@hotmail.com>
2019-05-09 21:44:11 -0700

    update storybook to 5.x; update babel, eslint, react in devDep

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,12 @@
-import { configure } from '@storybook/react'
-import { setDefaults } from '@storybook/addon-info'
+import { configure, addDecorator } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
 
 // Docs: https://github.com/storybooks/storybook/tree/master/addons/info
-setDefaults({
-  header: true,
-})
+addDecorator(
+  withInfo({
+    header: true,
+  }),
+)
 
 configure(() => {
   require('../stories/index')
--- stories/index.js
+++ stories/index.js
@@ -1,40 +1,27 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { withInfo } from '@storybook/addon-info'
 import { action } from '@storybook/addon-actions'
 import Player from '../src'
 
 const SAMPLE_MP3_URL = 'https://ia802508.us.archive.org/5/items/testmp3testfile/mpthreetest.mp3'
 
 storiesOf('Player', module)
-  .add(
-    'Basic',
-    withInfo(`Basic usage
-`)(() => <Player src={SAMPLE_MP3_URL} volume={0.8} />),
-  )
-  .add(
-    'Auto Play',
-    withInfo(`Auto Play
-`)(() => <Player autoPlay progressUpdateInterval={100} src={SAMPLE_MP3_URL} />),
-  )
-  .add(
-    'Action Logger',
-    withInfo(`Auto Play
-`)(() => (
-      <Player
-        onAbort={action('onAbort')}
-        onCanPlay={action('onCanPlay')}
-        onCanPlayThrough={action('onCanPlayThrough')}
-        onEnded={action('onEnded')}
-        onError={action('onError')}
-        onListen={action('onListen')}
-        onPause={action('onPause')}
-        onPlay={action('onPlay')}
-        onDragStart={action('onDragStart')}
-        onDragMove={action('onDragMove')}
-        onDragEnd={action('onDragEnd')}
-        progressUpdateInterval={100}
-        src={SAMPLE_MP3_URL}
-      />
-    )),
-  )
+  .add('Basic', () => <Player src={SAMPLE_MP3_URL} volume={0.8} />)
+  .add('Auto Play', () => <Player autoPlay progressUpdateInterval={100} src={SAMPLE_MP3_URL} />)
+  .add('Action Logger', () => (
+    <Player
+      onAbort={action('onAbort')}
+      onCanPlay={action('onCanPlay')}
+      onCanPlayThrough={action('onCanPlayThrough')}
+      onEnded={action('onEnded')}
+      onError={action('onError')}
+      onListen={action('onListen')}
+      onPause={action('onPause')}
+      onPlay={action('onPlay')}
+      onDragStart={action('onDragStart')}
+      onDragMove={action('onDragMove')}
+      onDragEnd={action('onDragEnd')}
+      progressUpdateInterval={100}
+      src={SAMPLE_MP3_URL}
+    />
+  ))
