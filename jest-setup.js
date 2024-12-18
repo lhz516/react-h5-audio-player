13963e91387f7c403bed7bade273dcd66d74948b
Hanz Luo <lhz0516@gmail.com>
2024-12-18 11:35:36 -0800

    3.10.0 rc - due to React 19 won't pass test

--- jest-setup.js
+++ jest-setup.js
@@ -1,5 +1,5 @@
 import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import Adapter from '@cfaester/enzyme-adapter-react-18'
 
 // Configure the enzyme adapter.
 configure({ adapter: new Adapter() })
